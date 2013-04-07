f0e1ecd1e7ceaa9ee14c34c1a93bfc877a2f3d52
Ozgur Yogurtcu <oyogurtcu@gmail.com>
2013-04-07 19:40:02 -0400

    Style changes

--- modules/community/ecrafting/lib/ui.extensions.js
+++ modules/community/ecrafting/lib/ui.extensions.js
@@ -10,25 +10,35 @@
  * Menu rendering functions
  * Over-write to enable custom menu rendering
  */
+
+menu.cls = "nav nav-pils"
+
 menu.prototype.startTag = function () {
-  return "<ul id='" + this.name + "-menu' class='menuxx" + (this.cls ? ' ' + this.cls : '') + "'>";
+  return "<ul id='" + this.name + "-menu' class='nav nav-pills'>";
 };
 menu.prototype.endTag = function () {
   return "</ul>";
 };
 menu.prototype.menuStartTag = function (menu, selected) {
   var menuItemTagId = menu.path.replace(/\//g, '-') + "-menu-item";
-  return "<li id='" + menuItemTagId + "' class='" + this.name + "-menu-item" + selected + "'>";
+  menu.started = true;
+  return "<li id='" + menuItemTagId + "' class='dropdown " + selected + "'>";
 };
 menu.prototype.menuLinkTag = function (req, menu, selected) {
   var popup = menu.popup ? 'popupMenu' : '';
-  return "<a href='" + menu.url + "' title='" + req.t(menu.description) + "' class='" + popup + " " + this.name + "-menu-link" + selected + (menu.cls ? " " + menu.cls : "") + "'>" + req.t(menu.name) + "</a>";
+  calipso.debug("Type, Menu ", menu.type + ' ' + menu.path);
+  if (menu.started) {
+    return "<a href='" + menu.url + "' title='" + req.t(menu.description) + "'>" + req.t(menu.name) + "</a>";
+  }
+  return "<a href='" + menu.url + "' title='" + req.t(menu.description) + "' data-toggle='dropdown'>" + req.t(menu.name) + "</a>";
+//  return "<a href='" + menu.url + "' title='" + req.t(menu.description) + "' class='" + popup + " " + this.name + "-menu-link" + selected + (menu.cls ? " " + menu.cls : "") + "'>" + req.t(menu.name) + "</a>";
 };
 menu.prototype.menuEndTag = function (menu) {
+  menu.started = false;
   return "</li>";
 };
 menu.prototype.childrenStartTag = function () {
-  return "<ul>";
+  return "<ul class='dropdown-menu'>";
 };
 menu.prototype.childrenEndTag = function () {
   return "</ul>";
