16371f0511941f381ad382a27eeca4e550b6445f
Ozgur Yogurtcu <oyogurtcu@gmail.com>
2014-01-25 19:00:05 -0500

    Registration email issue.

--- modules/core/user/user.js
+++ modules/core/user/user.js
@@ -926,10 +926,10 @@ function registerUser(req, res, template, block, next) {
             return;
           } else {
             calipso.e.post_emit('USER_CREATE', u);
-            calipso.e.post_emit('USER_ACTIVATIONMAIL', u);
             if (!res.noRedirect) {
               req.flash('info', req.t('We sent you an activation email. Please check your mail and click on the activation link.'));
               res.redirect('/user/profile/' + u.username);
+              calipso.e.post_emit('USER_ACTIVATIONMAIL', u);
             }
             return;
           }
