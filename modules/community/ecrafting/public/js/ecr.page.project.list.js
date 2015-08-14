64e2898d9e599a741a42fb564c3c704eb048ee45
eylemaydin <eylemaydin26@gmail.com>
2015-08-14 20:37:59 +0300

    https problems

--- modules/community/ecrafting/public/js/ecr.page.project.list.js
+++ modules/community/ecrafting/public/js/ecr.page.project.list.js
@@ -18,7 +18,7 @@ ecr.page.Page = function () {
 
 		apiWrapper.apiCall(command, parameters, null, function (data, jqXhr) {
 			$.each(data, function(index, project) {
-				project.image = "http://placehold.it/100x100/95A5A6/fff/&text=P";
+				project.image = "//placehold.it/100x100/95A5A6/fff/&text=P";
 				if (project.media && project.media[0]) {
 					project.image = '/api/media/' + project.media[0] + ".small";
 				}
