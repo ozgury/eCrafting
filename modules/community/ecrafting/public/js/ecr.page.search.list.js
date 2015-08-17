0ecbe1abd4a93ba716499ede7fd042fb3a259925
eylemaydin <eylemaydin26@gmail.com>
2015-08-17 21:39:58 +0300

    undo http problems commit which SHA is 64e2898d9e599a741a42fb564c3c704eb048ee45

--- modules/community/ecrafting/public/js/ecr.page.search.list.js
+++ modules/community/ecrafting/public/js/ecr.page.search.list.js
@@ -21,7 +21,7 @@ ecr.page.Page = function () {
 
         apiWrapper.apiCall(command, parameters, null, function (data, jqXhr) {
             $.each(data, function(index, project) {
-                project.image = "//placehold.it/100x100/95A5A6/fff/&text=P";
+                project.image = "http://placehold.it/100x100/95A5A6/fff/&text=P";
                 if (project.media && project.media[0]) {
                     project.image = '/api/media/' + project.media[0] + ".small";
                 }
