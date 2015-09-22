cb9ec89678c24a4ea56d7c7c4fd53d59176c539c
eylemaydin <[eylemaydin26@gmail.com]>
2015-09-22 20:19:30 +0300

    Forgot Password issue

--- modules/core/user/public/js/ecr.page.login.js
+++ modules/core/user/public/js/ecr.page.login.js
@@ -12,31 +12,19 @@ if (typeof (ecr.page) == 'undefined') {
 ecr.page.Page = function () {
 	var apiWrapper = new ecr.ApiWrapper();
 
-
-	function validateEmail(email) { 
-	    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	    return re.test(email);
-	} 
-
 	this.initialize = function () {
 		$('#forgotPassword').click(function () {
-			var parameters = null;
-			var email = $('#user\\[username\\]').val();
 
-			if (!email && email.length == 0) {
-				ecr.app.userError("Please enter your email address to reset your password.");
-				return;
-			}
-
-			if (!validateEmail(email)) {
-				ecr.app.userError("Please enter a valid email address to reset your password.");
-				return;
-			}
-
-			apiWrapper.call('/user/resetpassword', '{ "username": "' + email + '" }', 'POST', function (result, other, exception) {
-				ecr.app.userSuccess("We sent you an email to reset your password.");
-			}, null)
-			return false;
+			bootbox.prompt("Please enter your username to reset your password.", function(result) {
+				if (result === null) {
+					ecr.app.userError("Please enter your username to reset your password.");
+					return;
+				} else {
+					apiWrapper.call('/user/resetpassword', '{ "username": "' + result + '" }', 'POST', function (result, other, exception) {
+						ecr.app.userSuccess("We sent you an email to reset your password.");
+					}, null)
+				}
+			});
 		});
 	}
 };
